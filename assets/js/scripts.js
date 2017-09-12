9b7993f94d5a450377acedff418df7d5599a94b0
Daniel Segovia <segoarce90@gmail.com>
2017-09-12 14:39:23 -0500

    fade in of btn-shop

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -2,7 +2,8 @@ $(document).ready(() => {
   console.log('ready!');
 
   $(document).mousemove(function(event) {
-    $('#desktop-header').css('visibility', 'visible').addClass('fadeIn')
+    $('#desktop-header').css('visibility', 'visible').addClass('fadeIn');
+    $('.btn-shop').css('visibility', 'visible').addClass('fadeIn');
   })
 
 })
