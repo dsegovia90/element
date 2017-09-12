8e18bbe50a26ca112c99bb914786b443afe98ed8
Daniel Segovia <segoarce90@gmail.com>
2017-09-12 16:43:48 -0500

    fadeOut logo div

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,7 +1,9 @@
 $(document).ready(() => {
+  $('body').css('visibility', 'visible');
   console.log('ready!');
 
   $(document).mousemove(function(event) {
+    $('#big-element-logo-div').addClass('fadeOut');
     $('.fadeIn-mouse').css('visibility', 'visible').addClass('fadeIn');
   })
 
