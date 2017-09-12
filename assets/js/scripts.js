ad9b429966e24982ffe14f2a071c731f449500c9
Daniel Segovia <segoarce90@gmail.com>
2017-09-12 17:16:28 -0500

    visual changes to code

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -3,12 +3,14 @@ $(document).ready(() => {
   console.log('ready!');
 
   $(document).mousemove(function(event) {
-    $('#big-element-logo-div').addClass('fadeOut').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', poop);
+    $('#big-element-logo-div')
+      .addClass('fadeOut')
+      .one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', hideAndFadeIn);
   })
   
 })
 
-function poop() {
+function hideAndFadeIn() {
   $('#big-element-logo-div').hide();
   $('.fadeIn-mouse').css('visibility', 'visible').addClass('fadeIn');
 }
